ab088e0f70772196e0c7719b7f4dc17648e0ac8e
Alika Choo <alikachoo5@gmail.com>
2023-09-07 17:17:49 +0800

    add site seo

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import { Seo } from '../components/seo';
 
 export default function AboutPage() {
   return (
-    <main>
-      <h1>About this site</h1>
-      <Link to="/">Back to Home</Link>
-    </main>
+    <>
+      <Seo
+        title="About This Site"
+        description="More information about this site"
+      />
+      <main>
+        <h1>About this site</h1>
+        <Link to="/">Back to Home</Link>
+      </main>
+    </>
   );
 }
